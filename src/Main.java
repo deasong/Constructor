3b37374e1ea067d942260787e9a4333ebf6a5c34
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-01-15 18:03:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static <Classmate> void main(String[] args) {
+
+        int i1 = 324;
+        Classmate classmate= (Classmate) new Class(123,"the car"," "+i1);
+        System.out.println(classmate);
+    }
+}
