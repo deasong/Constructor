b18a3c813d8ddcd1ce6df539e4fbe6f0132d561e
Asus Zenbook <anapiiaevsandary@gmail.com>
2024-01-18 18:08:54 +0300

    Class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-public class Main {
-    public static <Classmate> void main(String[] args) {
 
-        int i1 = 324;
-        Classmate classmate= (Classmate) new Class(123,"the car"," "+i1);
-        System.out.println(classmate);
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("------------FIGURA------------------------------\n");
+        Figure figure = new Figure(2, 2.5);
+
+        System.out.println(figure);
+        System.out.println("---------ROUND----------------------------------\n");
+        Round round = new Round(3, 2.5, 3);
+        System.out.println(round);
+        System.out.println("-----------RECANGLE-----------------------------\n");
+        Rectangle rectangle = new Rectangle(4, 4.5);
+        System.out.println(rectangle);
+        System.out.println("-----------TRIANGLE----------------------------\n");
+        Triangle triangle = new Triangle(6, 4.5);
+        System.out.println(triangle);
+
     }
+
 }
